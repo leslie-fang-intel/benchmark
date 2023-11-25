d26626f1f6b48642ce136b45bd0a38319c8935d2
jianan-gu <jianan.gu@intel.com>
2023-11-24 16:38:42 -0800

    Enable QAT

--- torchbenchmark/util/backends/torchdynamo.py
+++ torchbenchmark/util/backends/torchdynamo.py
@@ -142,7 +142,7 @@ def apply_torchdynamo_args(model: 'torchbenchmark.util.model.BenchmarkModel', ar
     torchdynamo.reset()
 
 def enable_inductor_quant(model: 'torchbenchmark.util.model.BenchmarkModel', is_qat: 'bool'=False):
-    from torch.ao.quantization.quantize_pt2e import prepare_pt2e, convert_pt2e
+    from torch.ao.quantization.quantize_pt2e import prepare_pt2e, convert_pt2e, prepare_qat_pt2e
     import torch.ao.quantization.quantizer.x86_inductor_quantizer as xiq
     from torch._export import capture_pre_autograd_graph, dynamic_dim
     module, example_inputs = model.get_module()
@@ -150,12 +150,17 @@ def enable_inductor_quant(model: 'torchbenchmark.util.model.BenchmarkModel', is_
     quantizer = xiq.X86InductorQuantizer()
     quantizer.set_global(xiq.get_default_x86_inductor_quantization_config(is_qat=is_qat))
     # Generate the FX Module
+    if is_qat:
+        module.train()
     exported_model = capture_pre_autograd_graph(
             module,
             example_inputs
         )
     # PT2E Quantization flow
-    prepared_model = prepare_pt2e(exported_model, quantizer)
+    if is_qat:
+        prepared_model = prepare_qat_pt2e(exported_model, quantizer)
+    else:
+        prepared_model = prepare_pt2e(exported_model, quantizer)
     # Calibration
     if is_qat:
         model.example_outputs = (torch.rand_like(module(*example_inputs)), )
@@ -164,6 +169,8 @@ def enable_inductor_quant(model: 'torchbenchmark.util.model.BenchmarkModel', is_
         model.train()
     else:
         prepared_model(*example_inputs)
-    converted_model = convert_pt2e(prepared_model)
-    torch.ao.quantization.move_exported_model_to_eval(converted_model)
-    model.set_module(converted_model)
+
+    with torch.no_grad():
+        converted_model = convert_pt2e(prepared_model)
+        torch.ao.quantization.move_exported_model_to_eval(converted_model)
+        model.set_module(converted_model)
